cb988677fc66d99d9ef07d5e158ac99a78b97dec
MoPo <74471449+MoPoRem@users.noreply.github.com>
2023-04-06 16:24:54 +0200

    Create ApiClient.d.ts

---
+++ ApiClient.d.ts
@@ -0,0 +1,19 @@
+export default ApiClient;
+declare class ApiClient {
+    constructor(baseURL: String, token: String);
+    baseURL: string;
+    resourceURL: string;
+    headers: {
+        Authorization: string;
+    };
+    _fetch(url: string, options?: {}): Promise<any>;
+    from(modelName: string): ApiClient;
+    call(name: string, data: object): Promise<any>;
+    all(): Promise<any>;
+    filter(params: object): Promise<any>;
+    create(data: object): Promise<any>;
+    get(pk: number): Promise<any>;
+    update(pk: number, data: object): Promise<any>;
+    patch(pk: number, data: object): Promise<any>;
+    delete(pk: number): Promise<any>;
+}
