36cf6afa8ac2d3d1728bdbf4677274dcbdda47c3
MoPo <pooriavrj@gmail.com>
2024-01-22 11:09:46 +0100

    chore: formatting changes

--- ApiClient.js
+++ ApiClient.js
@@ -1,102 +1,113 @@
 class ApiClient {
-
-    constructor(baseURL, token) {
-    this.baseURL = baseURL
-    this.resourceURL = null
+  constructor(baseURL, token) {
+    this.baseURL = baseURL;
+    this.resourceURL = null;
     if (token) {
       this.headers = {
         Authorization: `Token ${token}`,
-      }
+      };
     } else {
-      this.headers = {}
+      this.headers = {};
     }
-    this.pk = null
+    this.pk = null;
   }
 
-  _fetch(url, options = {}, pk=undefined) {
-    url = this._constructUrlFromPk(url, pk)
+  _fetch(url, options = {}, pk = undefined) {
+    url = this._constructUrlFromPk(url, pk);
     return fetch(url, {
       ...options,
       headers: {
         ...options.headers,
         ...this.headers,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    }).then(response => response.json())
+    }).then((response) => response.json());
   }
 
   _constructUrlFromPk(url, pk) {
     if (pk) {
-      return `${url}${pk}/`
+      return `${url}${pk}/`;
     }
-    return url
+    return url;
   }
-  
-  from (modelName){
-    if(modelName.includes('.')){
-      const[folder, model] = modelName.split('.')
-      this.resourceURL = `${this.baseURL}/${folder}/${model}/`
-    }else {
-      this.resourceURL = `${this.baseURL}/models/${modelName}/`
+
+  from(modelName) {
+    if (modelName.includes(".")) {
+      const [folder, model] = modelName.split(".");
+      this.resourceURL = `${this.baseURL}/${folder}/${model}/`;
+    } else {
+      this.resourceURL = `${this.baseURL}/models/${modelName}/`;
     }
-    return this
+    return this;
   }
-  
+
   call(name, data) {
-	  return this._fetch(`${this.baseURL}/functions/${name}/`, 
-		{ 
-			method: "POST", 
-			body: JSON.stringify({params: data})
-		})
+    return this._fetch(`${this.baseURL}/functions/${name}/`, {
+      method: "POST",
+      body: JSON.stringify({ params: data }),
+    });
   }
 
   all() {
-    return this._fetch(this.resourceURL)
+    return this._fetch(this.resourceURL);
   }
 
   filter(params) {
-    const url = new URL(this.resourceURL)
-    Object.entries(params).forEach(([key, val]) => url.searchParams.append(key, val))
-    return this._fetch(url)
+    const url = new URL(this.resourceURL);
+    Object.entries(params).forEach(([key, val]) =>
+      url.searchParams.append(key, val)
+    );
+    return this._fetch(url);
   }
 
   create(data) {
     return this._fetch(this.resourceURL, {
-      method: 'POST',
+      method: "POST",
       headers: this.headers,
       body: JSON.stringify(data),
-    })
+    });
   }
 
   get(pk) {
-    return this._fetch(this.resourceURL, {}, pk)
+    return this._fetch(this.resourceURL, {}, pk);
   }
 
   update(data, pk) {
-    return this._fetch(this.resourceURL, {
-      method: 'PUT',
-      headers: this.headers,
-      body: JSON.stringify(data),
-    }, pk)
+    return this._fetch(
+      this.resourceURL,
+      {
+        method: "PUT",
+        headers: this.headers,
+        body: JSON.stringify(data),
+      },
+      pk
+    );
   }
 
   patch(data, pk) {
-    const url = `${this.resourceURL}${pk}/`
-    return this._fetch(this.resourceURL, {
-      method: 'PATCH',
-      headers: this.headers,
-      body: JSON.stringify(data),
-    }, pk)
+    const url = `${this.resourceURL}${pk}/`;
+    return this._fetch(
+      this.resourceURL,
+      {
+        method: "PATCH",
+        headers: this.headers,
+        body: JSON.stringify(data),
+      },
+      pk
+    );
   }
 
   delete(pk) {
-    const url = `${this.resourceURL}${pk}/`
-    return this._fetch(this.resourceURL, {
-      method: 'DELETE',
-      headers: this.headers,
-    }, pk)
+    const url = `${this.resourceURL}${pk}/`;
+    return this._fetch(
+      this.resourceURL,
+      {
+        method: "DELETE",
+        headers: this.headers,
+      },
+      pk
+    );
   }
-
 }
 
 module.exports = ApiClient;
